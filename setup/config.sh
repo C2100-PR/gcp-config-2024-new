f0748e778ceba5a2a245f8fa5e74dacdde840a49
Phillip Corey Roark <pr@coaching2100.com>
2024-12-29 21:22:37 -0600

    Add initial configuration script

---
+++ setup/config.sh
@@ -0,0 +1,7 @@
+# GCP Identity Configuration Script
+gcloud services enable \
+  cloudresourcemanager.googleapis.com \
+  iam.googleapis.com \
+  iamcredentials.googleapis.com \
+  sts.googleapis.com \
+  --project=admin-coaching2100
